5970f07d396a12de3ca608b73387c62513b126de
y2k <itwisterlx@gmail.com>
2019-07-08 16:43:27 +0300

    add ignore & refactoring

--- src/test/kotlin/y2k/litedb/LiteDbTest.kt
+++ src/test/kotlin/y2k/litedb/LiteDbTest.kt
@@ -2,13 +2,17 @@ package y2k.litedb
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import y2k.litedb.LiteDbTestUtils.User
+import y2k.litedb.LiteDbTestUtils.UserMeta
+import y2k.litedb.LiteDbTestUtils.mkRandomList
+import y2k.litedb.LiteDbTestUtils.querySuspend
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 import kotlin.random.Random
 
 class LiteDbTest {
 
-    @Test()
+    @Test
     fun `delete with filter should success`() = runTest {
         val filter: (User) -> Boolean = { it.id < 10 }
         val sqlFilter: UserMeta.() -> Tree = { id lt 10 }
@@ -20,16 +24,16 @@ class LiteDbTest {
 
         db.delete(UserMeta, sqlFilter)
 
-        val actual = query(db, UserMeta, EmptyFilter)
+        val actual = db.querySuspend(UserMeta, EmptyFilter)
         assertEquals(expected.filterNot(filter), actual)
     }
 
     @Test
     fun `eq filter should success`() = runTest {
         val example = mkRandomList()
-        assertQuery(example, { it.name == example[5].name }) {
-            name eq example[5].name
-        }
+        assertQuery(example,
+            { it.name == example[5].name },
+            { name eq example[5].name })
     }
 
     @Test
@@ -39,35 +43,35 @@ class LiteDbTest {
 
     @Test
     fun `test simple filter`() = runTest {
-        assertQuery(mkRandomList(), { it.id >= 5 }) {
-            id gtOrEq 5
-        }
+        assertQuery(mkRandomList(),
+            { it.id >= 5 },
+            { id gtOrEq 5 })
     }
 
     @Test
     fun `test complex filter`() = runTest {
-        assertQuery(mkRandomList(), { it.id <= 2 || it.id >= 8 }) {
-            or(
-                id ltOrEq 2,
-                id gtOrEq 8
-            )
-        }
+        assertQuery(mkRandomList(),
+            { it.id <= 2 || it.id >= 8 },
+            { or(id ltOrEq 2, id gtOrEq 8) })
     }
 
     private suspend fun assertQuery(expected: List<User>, filter: (User) -> Boolean, sqlFilter: UserMeta.() -> Tree) {
         val db = LiteDb(DesktopConnector(":memory:"))
         db.insertAll(UserMeta, expected)
 
-        val actual = query(db, UserMeta, sqlFilter)
+        val actual = db.querySuspend(UserMeta, sqlFilter)
 
         assertEquals(expected.filter(filter), actual)
     }
+}
 
-    private fun mkRandomList(): List<User> = List(20, ::mkRandomUser)
+object LiteDbTestUtils {
 
-    private suspend fun <M : Meta<T>, T : Any> query(db: LiteDb, meta: M, init: M.() -> Tree): List<T> =
+    fun mkRandomList(): List<User> = List(20, ::mkRandomUser)
+
+    suspend fun <M : Meta<T>, T : Any> LiteDb.querySuspend(meta: M, init: M.() -> Tree): List<T> =
         suspendCoroutine { continuation ->
-            db.query(meta, init, continuation::resume)
+            query(meta, init, continuation::resume)
         }
 
     private fun mkRandomUser(id: Int): User =
@@ -78,19 +82,19 @@ class LiteDbTest {
             City("name #${Random.nextInt(10000)}", emptyList()),
             Random.nextInt(150)
         )
+
+    object UserMeta : Meta<User> {
+        val id = filter<Int>("id")
+        val name = filter<String>("name")
+    }
+
+    data class User(
+        val id: Int,
+        val name: String,
+        val lang: String,
+        val city: City,
+        val age: Int
+    )
+
+    data class City(val name: String, val location: List<Float>)
 }
-
-object UserMeta : Meta<User> {
-    val id = filter<Int>("id")
-    val name = filter<String>("name")
-}
-
-data class User(
-    val id: Int,
-    val name: String,
-    val lang: String,
-    val city: City,
-    val age: Int
-)
-
-data class City(val name: String, val location: List<Float>)
